830b640c14a9252643f9396c6089a63014e7d946
Jens Billing <jensis92@hotmail.com>
2021-08-20 15:09:36 +0200

    add SpringFox dependency and config class

---
+++ src/main/java/com/example/pizzaorderservice/SpringFoxConfig.java
@@ -0,0 +1,24 @@
+package com.example.pizzaorderservice;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+@Configuration
+public class SpringFoxConfig {
+
+    // Documentation url : http://localhost:8082/v2/api-docs
+    // Swagger-UI : http://localhost:8082/swagger-ui/
+
+    @Bean
+    public Docket api(){
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .paths(PathSelectors.any())
+                .apis(RequestHandlerSelectors.any())
+                .build();
+    }
+}
